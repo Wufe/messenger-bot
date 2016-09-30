06dad1be6d0f7a29e6e6de13ccfd7e3615db024f
Wufe <wufe92@gmail.com>
2016-09-30 22:09:41 +0200

    added validation

--- src/app/Http/Controllers/FacebookWebhookController.php
+++ src/app/Http/Controllers/FacebookWebhookController.php
@@ -13,7 +13,19 @@ class FacebookWebhookController extends Controller
 	public function validateWebhook( Request $request ){
 
 		Log::info( $request->fullUrl()."\n".print_r($request->all(),true) );
-		return response()->json([ "status" => "ok", "fullUrl" => $request->fullUrl(), "all" => $request->all() ], 200);
+		$isHubModeValid = $request->has( "hub_mode" ) && $request->hub_mode === "subscribe";
+		$isConfigurationKeyValid = $request->has( "hub_verify_token" ) && $request->hub_verify_token === config("app.key");
+		$hasHubChallenge = $request->has( "hub_challenge" );
+		if( $isHubModeValid && $isConfigurationKeyValid && $hasHubChallenge ){
+			Log::info( "Validated webhook with challenge ".$request->hub_challenge."." );
+			return response( $request->hub_challenge );
+		}else{
+			Log::info( "Failed webhook validation." );
+			return response(403);
+		}
+		
+		//if( $request->has( "hub_mode" ))
+		//return response()->json([ "status" => "ok", "config" => config("app.key"), "fullUrl" => $request->fullUrl(), "all" => $request->all() ], 200);
 	}
  //    app.get('/webhook', function(req, res) {
 	//   if (req.query['hub.mode'] === 'subscribe' &&
